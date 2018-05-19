35f1ffe0420413cbb7bb340e3978835b5ea69592
Rachit Mishra <twntee@gmail.com>
2018-05-19 15:08:26 +0530

    Init commit

---
+++ app/src/main/java/in/ceeq/scrolldemo/MainActivity.kt
@@ -0,0 +1,16 @@
+package `in`.ceeq.scrolldemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        click_view.setOnClickListener {
+//            Toast.makeText(this, "Clicked!!", Toast.LENGTH_LONG).show()
+//        }
+    }
+}
